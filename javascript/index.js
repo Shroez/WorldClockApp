bb0ea19cd523dac4872018a46a5db48d4d1d0c61
Shroez <Shamahararodriguez@gmail.com>
2025-09-18 23:20:13 -0700

    added the functionality of the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,43 +1,46 @@
 function updateTime() {
   //Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
-  losAngelesTimeElement.innerHTML = ` ${losAngelesTime.format("h:mm:ss")}
-    <small> ${losAngelesTime.format("A")} </small>`;
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
+    losAngelesTimeElement.innerHTML = ` ${losAngelesTime.format("h:mm:ss")}
+      <small> ${losAngelesTime.format("A")} </small>`;
+  }
   //Mexico City
   let mexicoCityElement = document.querySelector("#mexico-city");
-  let mexicoCityDateElement = mexicoCityElement.querySelector(".date");
-  let mexicoCityTimeElement = mexicoCityElement.querySelector(".time");
-  let mexicoCityTime = moment().tz("America/Mexico_City");
+  if (mexicoCityElement) {
+    let mexicoCityDateElement = mexicoCityElement.querySelector(".date");
+    let mexicoCityTimeElement = mexicoCityElement.querySelector(".time");
+    let mexicoCityTime = moment().tz("America/Mexico_City");
 
-  mexicoCityDateElement.innerHTML = mexicoCityTime.format("MMMM Do, YYYY");
-  mexicoCityTimeElement.innerHTML = ` ${mexicoCityTime.format("h:mm:ss")}
-    <small> ${mexicoCityTime.format("A")} </small>`;
+    mexicoCityDateElement.innerHTML = mexicoCityTime.format("MMMM Do, YYYY");
+    mexicoCityTimeElement.innerHTML = ` ${mexicoCityTime.format("h:mm:ss")}
+      <small> ${mexicoCityTime.format("A")} </small>`;
+  }
+}
 
-  /// Peru
-  let peruElement = document.querySelector("#peru");
-  let peruDateElement = peruElement.querySelector(".date");
-  let peruTimeElement = peruElement.querySelector(".time");
-  let peruTime = moment().tz("America/Lima");
-
-  peruDateElement.innerHTML = peruTime.format("MMMM Do, YYYY");
-  peruTimeElement.innerHTML = peruTime.format("h:mm:ss [<small>]A[</div>]");
-
-  //Argentina
-  let argentinaElement = document.querySelector("#argentina");
-  let argentinaDateElement = argentinaElement.querySelector(".date");
-  let argentinaTimeElement = argentinaElement.querySelector(".time");
-  let argentinaTime = moment().tz("America/Argentina/Buenos_Aires");
-
-  argentinaDateElement.innerHTML = argentinaTime.format("MMMM Do, YYYY");
-  argentinaTimeElement.innerHTML = argentinaTime.format(
-    "h:mm:ss [<small>]A[</div>]"
-  );
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city"  >
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("h:mm:ss")}
+           <small> ${cityTime.format("A")}</small>
+           </div>
+        </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
